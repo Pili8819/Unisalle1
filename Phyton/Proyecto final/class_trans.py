44bbc161803308a6611b4ad136834db222f7e8d5
dev_mneusa38 <mneusa38@unisalle.edu.co>
2026-05-19 21:15:22 -0500

    Proyecto final

---
+++ Phyton/Proyecto final/class_trans.py
@@ -0,0 +1,21 @@
+class Transaccion:
+    def __init__(self, fecha, categoria, monto, tipo, descripcion):
+        self.fecha = fecha
+        self.categoria = categoria
+        self.monto = float(monto)
+        self.tipo = tipo.lower()
+        self.descripcion = descripcion
+
+    def es_gasto(self):
+        return self.tipo == "expense"
+
+    def es_ingreso(self):
+        
+        return self.tipo == "income"
+
+    def __str__(self):
+        return (
+            f"{self.fecha} | {self.categoria} | "
+            f"{self.monto} | {self.tipo} | {self.descripcion}"
+        )
+    
